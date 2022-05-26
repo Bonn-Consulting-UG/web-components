1e3944855da6d738e5c8c68a9b5c083239adce03
Stefan Scheifel <stefan.scheifel@me.com>
2022-05-26 14:17:17 +0200

    Added Calendar

---
+++ src/components/calendar/component.ts
@@ -0,0 +1,17 @@
+
+import { css } from '@lion/core';
+import { LionCalendar } from '@lion/Calendar';
+
+ 
+export class BcgCalendar extends LionCalendar {
+    static get styles() {
+      return [
+        ...super.styles,
+        css`
+
+        }`,
+      ];
+    }
+  }
+
+
---
+++ src/components/calendar/index.ts
@@ -0,0 +1,5 @@
+/* eslint-disable import/extensions */
+import { BcgCalendar } from './component'
+
+window.customElements.define('bcg-calendar', BcgCalendar);
+
