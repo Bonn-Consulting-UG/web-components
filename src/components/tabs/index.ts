65cec97e59b81d81b2545dc6109c524b5001fcf2
Stefan Scheifel <stefan.scheifel@me.com>
2022-05-31 16:33:53 +0200

    push unsaved file

--- src/components/tabs/index.ts
+++ src/components/tabs/index.ts
@@ -1,5 +1,5 @@
 /* eslint-disable import/extensions */
 import { BcgTabs } from './tabs';
-import { componentNames } from '../../config';
+import { componentNames } from '../../utils/config';
 
 window.customElements.define(componentNames.tabs, BcgTabs);
