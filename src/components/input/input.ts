84b8c8d8c333a81ce59cbefab960207319ee36ac
Stefan Scheifel <stefan.scheifel@me.com>
2022-06-30 14:59:20 +0200

    fix import typo

--- src/components/input/input.ts
+++ src/components/input/input.ts
@@ -1,4 +1,4 @@
-import { LionInput } from '@lion/Input';
+import { LionInput } from '@lion/input';
 import { html, ScopedElementsMixin, LitElement, css } from '@lion/core';
 
 export class BcgInput extends ScopedElementsMixin(LitElement) {
