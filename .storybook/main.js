e2c9fbf9701a61c3774ab63fcdbbbe19ca084ef1
Stefan Scheifel <stefan.scheifel@me.com>
2022-06-30 15:54:07 +0200

    refactor storybook bonfig

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,3 @@
 module.exports = {
-  stories: ['../src/components/**/docs/*.stories.{js,md,mdx,ts}'],
-  babel: async options => ({
-    options: {
-      ...options,
-      plugins: [
-        ...options.plugins,
-        [
-          '@babel/plugin-proposal-decorators',
-          {
-            decoratorsBeforeExport: true,
-          },
-        ],
-        ['@babel/plugin-proposal-class-properties', { loose: false }],
-        ['@babel/plugin-proposal-private-methods', { loose: false }],
-      ],
-    },
-  }),
+  stories: ['../dist/**/**/docs/*.stories.{js,md,mdx}'],
 };
