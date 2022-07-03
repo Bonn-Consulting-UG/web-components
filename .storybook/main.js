e05e74de1c39da330b126ecb6e3682a4890a60d6
Stefan Scheifel <stefan.scheifel@me.com>
2022-07-03 18:00:44 +0200

    disable shortcuts

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,11 @@
 module.exports = {
   stories: ['../dist/**/**/docs/*.stories.{js,md,mdx}'],
+  addon: [
+    {
+      name: '@storybook/addons',
+      options: {
+        enableShortcuts: false,
+      },
+    },
+  ],
 };
